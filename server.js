30c7e3c9597d5c155868238c3759bb57e1c9e4db
zenglenn42 <gsnospampls@pobox.com>
2017-01-15 18:32:50 -0600

    clean up model

--- server.js
+++ server.js
@@ -8,13 +8,29 @@ var PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + "/app/public"));
 
 // Respond to survey submit button.
-app.get("/survey", function(req, res){
-	// stubbed out for now
-	console.log(req.query);
-	res.end("got form");
+app.get("/survey", function(req, res) {
+	var mbtiType = scoreSurvey(req.query);
+	res.end("You are a " + mbtiType);
 });
 
 // Listen for incoming requests from the client.
 app.listen(PORT, function() {
 	console.log("App listening on PORT:", PORT);
 });
+
+function scoreSurvey(survey) {
+	var results = {};
+	for (question in survey) {
+		var answer = survey[question];
+		var letter = answer.split(":")[0];
+		var weight = answer.split(":")[1];
+		results[letter] = weight;
+	}
+	var mbtiType = "";
+	mbtiType += (results.e >= results.i) ? 'e' : 'i';
+	mbtiType += (results.s >= results.n) ? 's' : 'n';
+	mbtiType += (results.f >= results.t) ? 'f' : 't';
+	mbtiType += (results.p >= results.j) ? 'p' : 'j';
+	console.log(mbtiType);
+	return mbtiType;
+}
