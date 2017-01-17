23a512179533569781b1a4772b3d0c0b9279adb2
zenglenn42 <gsnospampls@pobox.com>
2017-01-17 15:55:12 -0600

    fix /survey post route by adding body parser for incoming survey json

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var fs = require("fs");
+var bodyParser = require("body-parser");
 var mbtiModel = require("./app/model/model");
 
 var app = express();
@@ -8,18 +8,39 @@ var PORT = process.env.PORT || 8080;
 // Make public files available to the client.
 //app.use(express.static(__dirname + "/app/public"));
 
-// Respond to survey submit button.
+// Populate opening page with survey for now.
 app.get("/", function(req, res) {
-	var html = mbtiModel.survey.buildSurveyHtml("/survey", "get");
-	console.log(html);
+	var html = mbtiModel.survey.buildSurveyHtml("/survey", "post");
 	res.end(html);
 });
 
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse application/json 
+app.use(bodyParser.json())
+
+// Process survey once user presses 'submit' button.
+app.post("/survey", function(req, res) {
+	var mbtiType = mbtiModel.survey.scoreSurvey(req.body);
+	if (mbtiType) {
+		res.end("You are an " + mbtiType);
+	} else {
+		res.end("Unable to determine your Myers-Briggs personality type.");
+	}
+});
+
+/*
 // Respond to survey submit button.
 app.get("/survey", function(req, res) {
 	var mbtiType = mbtiModel.survey.scoreSurvey(req.query);
-	res.end("You are an " + mbtiType);
+	if (mbtiType) {
+		res.end("You are an " + mbtiType);
+	} else {
+		res.end("Unable to determine your Myers-Briggs personality type.");
+	}
 });
+*/
 
 // Listen for incoming requests from the client.
 app.listen(PORT, function() {
